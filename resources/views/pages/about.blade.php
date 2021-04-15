6c079579441184362693188d75d155f401e4d49b
Elhadji BADJI <EBadji@pscd.local>
2021-04-15 10:51:30 -0400

    Add proper page titles

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 
+@section('title', 'About Us | Hello City ')
+    <h1> About Us </h1>
+
     <p> Built with &hearts; by THE NET TEACHERS </p>
     <p><a href="/">Return to page home </a></p>
 
