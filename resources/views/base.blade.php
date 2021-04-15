d83d16fbd00b163a6435c7ae67b589c9e43752da
Elhadji BADJI <EBadji@pscd.local>
2021-04-15 12:03:29 -0400

    Avoid duplication by using configuration variable

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,20 +4,24 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title', 'Hello City')</title>
+        <title>@yield('title', config('app.name'))</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
     <body class="antialiased">
-        
+
         @yield('content')
-    
+
+        <!-- en variable access to config  -->
+        {{config('app.name')}}
+        {{config('projet.slogan')}}
+
 
         <footer>
             <p>
-                &copy; Copyrigth {{date('Y')}} 
+                &copy; Copyrigth {{date('Y')}}
                 &middot; <a href="about-us">About Us  </a>
             </p>
         </footer>
