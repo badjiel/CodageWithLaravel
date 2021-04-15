9a79802a65922e7878f560e1ea60a0273079d181
Elhadji BADJI <EBadji@pscd.local>
2021-04-15 13:46:37 -0400

    Add images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,8 @@
 
 @section('content')
 
+    <img src="/images/images.jfif" alt="Quebec Flag">
     <h1>Hello from Quebec ! </h1>
     <p> It's curently {{ date('h:i A') }} </p>
-      
+
 @endsection
